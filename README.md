66e7c25c5b6fd4fc22a5038c2207b83d5f8053e7
aws-farrdoug <farrdoug@amazon.co.uk>
2021-05-25 14:39:39 +0100

    Working security hub generator script

--- README.md
+++ README.md
@@ -1,11 +1,32 @@
-## My Project
+## Security Hub Score Generator 
 
-TODO: Fill this README out!
+Please find `scoreGenerator.py`, a simple python script to generate security hub scores within your AWS account. Currently, there is no API to get this score so we have to generate it ourselves in the following way:   
 
-Be sure to:
+- Calculating a compliance readiness score by calling GetFindings
+- Aggregating by complianceStatus
+- Getting a summary for each technicalControlId 
+- Computing the number of passed divided by total controls.
 
-* Change the title in this README
-* Edit your repository description on GitHub
+Being about to programmatically get security hub scores can help customers who want to see at a high level how an environment can change over time or as a result to a deployment. This could be adapted to store these values in AWS parameter store to keep an audit of the Security Hub Score.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install boto3.
+
+```bash
+pip3 install boto3
+```
+
+## Usage
+
+```python
+export AWS_DEFAULT_REGION=eu-west-2
+
+python3 scoreGenerator.py my-profile
+{'cis-aws-foundations-benchmark': {'Score': 76}, 'aws-foundational-security-best-practices': {'Score': 88}}
+```
+
+Tested with Python 3.7
 
 ## Security
 
---
+++ scoreGenerator.py
@@ -0,0 +1,67 @@
+import sys
+import boto3
+import json
+
+def get_standards_status(clientSh):
+    securityHubFindings = clientSh.get_findings(MaxResults=100)
+    standardsDict = {}
+    # loop for pagination
+    while len(securityHubFindings)>0:
+        if 'NextToken' in securityHubFindings:
+            nextToken = securityHubFindings['NextToken']
+            findings = securityHubFindings['Findings']
+            securityHubFindings = clientSh.get_findings(
+                MaxResults=100, NextToken=nextToken)
+            for finding in findings:
+                standardsDict = build_standards_dict(finding, standardsDict)  # logic to build dictionary
+        else:
+            break  # none left then end
+    return standardsDict
+
+def build_standards_dict(finding, standardsDict):
+    if any(x in json.dumps(finding) for x in ['Compliance', 'ProductFields']):
+        if 'Compliance' in finding:
+            status = finding['Compliance']['Status']
+            prodField = finding['ProductFields']
+            if (finding['RecordState'] == 'ACTIVE'):  # ignore disabled controls
+                control = None
+                # get values, json differnt for controls...
+                if 'StandardsArn' in prodField:  # for aws fun
+                    control = prodField['StandardsArn']
+                    rule = prodField['ControlId']
+                elif 'StandardsGuideArn' in prodField:  # for cis fun
+                    control = prodField['StandardsGuideArn']
+                    rule = prodField['RuleId']
+                #ignore custom findings
+                if control is not None:
+                    controlName = control.split('/')[1]  # get readable name from arn
+                    if controlName not in standardsDict:
+                        standardsDict[controlName] = {rule: status} # add new in
+                    elif not (rule in standardsDict[controlName] and (status == 'PASSED')):  # no need to update if passed
+                        standardsDict[controlName][rule] = status
+    return standardsDict
+
+def generateScore(standardsDict):
+    resultDict = {}
+    for control in standardsDict:
+        passCheck = 0
+        totalControls = len(standardsDict[control])
+        passCheck = len({test for test in standardsDict[control] if standardsDict[control][test] == 'PASSED'})
+
+        # generate score
+        score = round(passCheck/totalControls * 100)  # generate score
+        resultDict[control] = {"Score": score} #build dictionary
+    return resultDict
+
+def main(argv):
+    # Pull the profile from the parameters passed in.
+    profile = argv[1]
+
+    #create boto securityhub client
+    client = (boto3.session.Session(profile_name=profile)).client('securityhub')
+
+    scores = generateScore(get_standards_status(client))
+    print(scores)
+
+if __name__ == '__main__':
+    main(sys.argv)
